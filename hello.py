813dc30731cc88ccfa54dc8c2567220614c63cbd
SoonjeChoi <suhnjea777@gmail.com>
2022-05-06 13:33:52 +0900

    15 print 'yeardream'

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 print('hello')
 for i in range(1, 20+1):
-    if i %3 == 0:
+    if i%15 ==0:
+        print('yeardream')
+    elif i %3 == 0:
         print('year')
     elif i%5 == 0:
         print('dream')
